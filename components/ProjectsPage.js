5cda040a53027337d5c41f5e4c1bfb22e1406fd1
Piyush55dude <piyushagarwalvo@gmail.com>
2020-12-06 20:22:39 +0530

    Update projects

--- components/ProjectsPage.js
+++ components/ProjectsPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import ProjectCard from "./ProjectCard";
 
 import styles from "../styles/Home.module.css";
+import { projects } from "../data";
 
 const insta = "./gif/1.gif";
 const covid = "./gif/2.gif";
@@ -12,26 +13,14 @@ export default function ProjectsPage() {
     <>
       <h1 className={styles.projectHeading}>Projects</h1>
       <div className={styles.cards}>
-        <ProjectCard
-          name="Instagram Clone"
-          image={insta}
-          description=" Instagram Clone built with React JS and Material UI in frontend and used Firebase for the backend. Users can SignUp and log in to their accounts and post pictures with a caption. They can interact with other users by commenting on their posts. Also, posts in the right row are using the Instagram API to fetch a post from my real Instagram account."
-          link="https://instagram-clone-react-d14b7.web.app/"
-        />
-        <ProjectCard
-          name="COVID-19 Tracker"
-          image={covid}
-          description="COVID-19 Pandemic Tracer web application built with React JS.
-              This project presents the global data in the form of no. of Infected, Recovered and Deceased along with the historical Line Chart data. Selecting the country from drop down list presents the data of that particular country along with thier respective Bar Graph."
-          link="https://piyush55dude.github.io/covid19_tracer/"
-        />
-        <ProjectCard
-          name="Budget Calculator"
-          image={budget}
-          description="Basic budget calculator app using functions of React Hooks.
-              Each component of the app is updated by using useState hook and to not lose the data when we reload the webpage, I have used the browser's local storage to store the data in cache with the help of useEffect hook as well."
-          link="https://piyush55dude.github.io/Budget-Calculator/"
-        />
+        {projects.map((project) => (
+          <ProjectCard
+            name={project.name}
+            image={project.image}
+            description={project.description}
+            link={project.link}
+          />
+        ))}
       </div>
     </>
   );
---
+++ data.js
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    name: "Instagram Clone",
+    image: "./gif/1.gif",
+    description:
+      " Instagram Clone built with React JS and Material UI in frontend and used Firebase for the backend. Users can SignUp and log in to their accounts and post pictures with a caption. They can interact with other users by commenting on their posts. Also, posts in the right row are using the Instagram API to fetch a post from my real Instagram account.",
+    link: "https://instagram-clone-react-d14b7.web.app/",
+  },
+  {
+    name: "Artixie Ecommerce Store",
+    image: "./gif/5.gif",
+    description:
+      "Selling Creative stuff for super creative beings.\n Full Stack Project made in React JS,Node JS, Express JS and Mongo DB. User can order any product and provide rating as well.Payment option include Paypal or debit / credit cards. Admin can mark products as delivered. ",
+    link: "https://github.com/piyush-eon/Artixie-ecommerce-store",
+  },
+  {
+    name: "COVID-19 Tracker",
+    image: "./gif/2.gif",
+    description:
+      "COVID-19 Pandemic Tracer web application built with React JS. \n This project presents the global data in the form of no. of Infected, Recovered and Deceased along with the historical Line Chart data. Selecting the country from drop down list presents the data of that particular country along with thier respective Bar Graph.",
+    link: "https://piyush-eon.github.io/covid19_tracer/",
+  },
+  {
+    name: "Budget Calculator",
+    image: "./gif/3.gif",
+    description:
+      "Basic budget calculator app using functions of React Hooks. \n Each component of the app is updated by using useState hook and to not lose the data when we reload the webpage, I have used the browser's local storage to store the data in cache with the help of useEffect hook as well.",
+    link: "https://piyush55dude.github.io/Budget-Calculator/",
+  },
+
+  {
+    name: "Note Zipper",
+    image: "./gif/4.gif",
+    description:
+      "Note Zipper provides you one safe place for all your notes. Full Stack Project made in React JS,Node JS, Express JS and Mongo DB. Login / signup feature keeps all your notes safe in our database.",
+    link: "https://github.com/piyush-eon/notezipper",
+  },
+];
